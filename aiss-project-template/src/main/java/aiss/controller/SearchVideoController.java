2f10dba8a49108ce035b17b4c7ad44666693167f
dsp18 <dsp18@LAPTOP-3A780JNL>
2020-05-02 11:58:46 +0200

    Primer commit

---
+++ aiss-project-template/src/main/java/aiss/controller/SearchVideoController.java
@@ -0,0 +1,78 @@
+package aiss.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import aiss.model.resources.YoutubeResource;
+import aiss.model.youtube.VideoSearch;
+import aiss.model.edamam.EdamamSearch;
+import aiss.model.edamam.Hit;
+import aiss.model.edamam.Recipe;
+import aiss.model.resources.EdamamResource;
+
+/**
+ * Servlet implementation class SearchVideoController
+ */
+public class SearchVideoController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private static final Logger log = Logger.getLogger(SearchController.class.getName());
+
+	public SearchVideoController() {
+		super();
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String query = request.getParameter("label");
+		String recipe = request.getParameter("recipe");
+		String uri = request.getParameter("uri");
+		EdamamResource edamam = new EdamamResource();
+		EdamamSearch resultados = edamam.getRecipes(query);
+		RequestDispatcher rd = null;
+		
+		List<Recipe> lista = new ArrayList<>();
+		for (Hit hit : resultados.getHits()) {
+			lista.add(hit.getRecipe());
+		}
+		Map<String, Recipe> map = new HashMap<>();
+		for (Recipe recipe2 : lista) {
+			map.put(recipe2.getUri(), recipe2);
+		}
+		Recipe receta2 = map.get(uri);
+		
+		// Search for videos in YouTube
+		log.log(Level.FINE, "Searching for Youtube videos that contain " + query);
+		YoutubeResource youtube = new YoutubeResource();
+		VideoSearch youtubeResults = youtube.getVideos(query);
+		
+		if (youtubeResults!=null) {
+			request.setAttribute("name", query);
+			request.setAttribute("recipe2", receta2);
+			request.setAttribute("videos", youtubeResults.getItems());
+			request.setAttribute("recipe", recipe);
+			request.setAttribute("uri", uri);
+			request.setAttribute("recetaIngredientes", map);
+			rd = request.getRequestDispatcher("/successVideo.jsp");
+		} else {
+			log.log(Level.SEVERE, "YouTube object: " + youtubeResults);
+			rd = request.getRequestDispatcher("/error.jsp");
+		}
+		rd.forward(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)	throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
---
+++ aiss-project-template/src/main/webapp/successVideo.jsp
@@ -0,0 +1,54 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page import="aiss.model.edamam.Recipe"%>
+<%@ page import="java.util.HashMap"%>
+<%@ page import="aiss.model.edamam.Recipe"%>
+<%@ page import="java.util.Map"%>
+<%@ page import="java.util.List"%>
+<%@ page import="aiss.model.edamam.Ingredient"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Tutorial de la receta</title>
+<link href="css/VideoStyle.css" rel="stylesheet">
+</head>
+
+<body>
+	<c:choose>
+		<c:when test="${not empty param.label}">
+			<c:forEach items="${requestScope.videos}" var="item">
+				<iframe id="video" width="880" height="495" src="https://www.youtube.com/embed/<c:out value="${item.id.videoId}"/>" frameborder="0" allowfullscreen></iframe>
+			<div class="receta">
+				<h2>
+					<c:out value="${requestScope.recipe2.label}" />
+				</h2>
+				<p>Ingredientes:</p>
+				<div class="ingredientes">
+					<c:forEach items="${requestScope.recipe2.ingredients}" var="ingredient">
+						<p>
+							<c:out value="${ingredient.text}"/>
+						</p>
+					</c:forEach>
+				</div>
+			</div>
+			</c:forEach>
+		</c:when>
+		<c:otherwise>
+			<span>No hay resultados.</span>
+		</c:otherwise>
+	</c:choose>
+	
+	<form id="guardarDrive" action="/NewRecipeController" method="post" accept-charset="UTF-8">
+		<input id="label" name="label" type="hidden"value="${recipes.recipe.label}" /> 
+		<input id= "recipes" name="recipes" type="hidden" value="${recipes}">
+		<input id="uriRecipe" name="uriRecipe" type="hidden"value="${recipes.recipe.uri}" /> 
+		<input id="searchQuery" name="searchQuery" type="hidden" value="${requestScope.query}" /> 
+		<input type="submit"class="btn btn-default" name="guardar" value="Guardar en google Drive">	
+	</form>
+	
+</body>
+
+</html>
